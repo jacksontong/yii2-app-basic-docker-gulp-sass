ff890cb98cb8143e75e02d67355949f1c21fe13b
Jackson Tong <tongtoan2704@gmail.com>
2017-10-08 23:05:21 +0700

    SASS Gulp

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -15,12 +15,12 @@ use yii\web\AssetBundle;
  */
 class AppAsset extends AssetBundle
 {
-    public $basePath = '@webroot';
-    public $baseUrl = '@web';
+    public $sourcePath = '@app/assets/dist';
     public $css = [
-        'css/site.css',
+        YII_ENV_DEV ? 'css/all.css' : 'css/all.min.css'
     ];
     public $js = [
+        YII_ENV_DEV ? 'js/all.js' : 'js/all.min.js'
     ];
     public $depends = [
         'yii\web\YiiAsset',
