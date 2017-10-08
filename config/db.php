db710b15748505caddb85ab201bcf4acc7769cd5
Jackson Tong <tongtoan2704@gmail.com>
2017-10-09 00:22:09 +0700

    test db configuration

--- config/db.php
+++ config/db.php
@@ -5,5 +5,6 @@ return [
     'dsn' => 'mysql:host=' . getenv('MYSQL_HOST') . ';dbname=' . getenv('MYSQL_DATABASE'),
     'username' => getenv('MYSQL_USER'),
     'password' => getenv('MYSQL_PASSWORD'),
+    'tablePrefix' => 'tbl_',
     'charset' => 'utf8',
 ];
